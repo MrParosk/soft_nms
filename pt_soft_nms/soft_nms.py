471a24e3ff0d56a48b4e3fc9881381f00cd8d6ba
MrParosk <erik.parlstrand@gmail.com>
2021-02-06 20:38:43 +0100

    Fixed some typos

--- pt_soft_nms/soft_nms.py
+++ pt_soft_nms/soft_nms.py
@@ -1,16 +1,16 @@
 import torch
 
 
-def soft_nms(boxes: torch.Tensor, scores: torch.Tensor, sigma: float, score_threshold: float):
+def soft_nms(boxes: torch.Tensor, scores: torch.Tensor, sigma: float, score_threshold: float = 0.0):
     """
-    Performs soft-nms on the boxes (with the (Gaussian function).
+    Performs soft-nms on the boxes (with the Gaussian function).
 
     Args:
         boxes (Tensor[N, 5]): Boxes to perform NMS on. They are expected to be in
            (x_min, y_min, x_max, y_max) format.
         scores (Tensor[N]): Scores for each one of the boxes
         sigma (float): The sigma parameter described in the paper which controls 
-        score_threshold: 
+        score_threshold: Will filter out all updated-scores which has value than score_threshold
     Returns:
         updated_scores (Tensor): float tensor with the updated scores, i.e.
             the scores after they have been decreased according to the overlap, 
